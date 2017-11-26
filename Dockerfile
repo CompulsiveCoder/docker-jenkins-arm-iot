e398806f5ace86b6434b60ac108885bf2574426c
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-26 07:42:16 +0000

    Updates and fixes

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ RUN apt-get update && apt-get install -y python mono-complete
 
 RUN python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
 
+RUN apt-get update && apt-get install -y libltdl7 && rm -rf /var/lib/apt/lists/*
+
 USER ${user}
 
 ENTRYPOINT ["/bin/tini", "--", "/usr/local/bin/jenkins.sh"]
--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
-#docker run -ti jenkins-arm-iot
+# Must run as sudo
 
-# No USB devices
-sudo docker run -it --restart always -u $USER -p 8080:8080 -p 50000:50000 -v /home/j/jenkins:/var/jenkins_home  jenkins-arm-iot
+# No USB devices without docker support
+#sudo docker run -it --restart always -u $USER -p 8080:8080 -p 50000:50000 -v /home/j/jenkins:/var/jenkins_home  jenkins-arm-iot
 
-# 2 USB devices
-#sudo docker run -dit --restart always -u $USER -p 8080:8080 -p 50000:50000 --device /dev/ttyUSB0 --device /dev/ttyUSB1 -v /home/j/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) reverie/armhf-jenkins
+# No USB devices with docker support
+sudo docker run -it --restart always -p 8080:8080 -p 50000:50000 -v /home/j/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker jenkins-arm-iot
+
+# 2 USB devices with docker support
+#sudo docker run -it --restart always -p 8080:8080 -p 50000:50000 -v /home/j/jenkins:/var/jenkins_home --device /dev/ttyUSB0 --device /dev/ttyUSB1 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker jenkins-arm-iot
 
