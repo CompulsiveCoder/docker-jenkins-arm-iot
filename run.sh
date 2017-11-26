63b50533a61322419a9ad44ab7a808644c79936b
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-26 07:05:26 +0000

    Updates and fixes

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-docker run -ti jenkins-arm-iot
+#docker run -ti jenkins-arm-iot
+
+# No USB devices
+sudo docker run -it --restart always -u $USER -p 8080:8080 -p 50000:50000 -v /home/j/jenkins:/var/jenkins_home  jenkins-arm-iot
+
+# 2 USB devices
+#sudo docker run -dit --restart always -u $USER -p 8080:8080 -p 50000:50000 --device /dev/ttyUSB0 --device /dev/ttyUSB1 -v /home/j/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) reverie/armhf-jenkins
+
