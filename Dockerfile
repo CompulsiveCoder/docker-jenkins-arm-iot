fa2e0b973a5f595a6d29936d5e135c432725f349
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-10 01:14:20 +0000

    Removed unneeded packages

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM reverie/armhf-jenkins
 
 USER root
 
-RUN apt-get update && apt-get install -y python build-essential libltdl7 mono-complete && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y nano python python-pip build-essential && rm -rf /var/lib/apt/lists/*
 
 RUN python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
 
--- run.sh
+++ run.sh
@@ -6,6 +6,3 @@
 # No USB devices with docker support
 sudo docker run -it --restart always -p 8080:8080 -p 50000:50000 -v /home/j/jenkins:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker jenkins-arm-iot
 
-# 2 USB devices with docker support
-#sudo docker run -it --restart always -p 8080:8080 -p 50000:50000 -v /home/j/jenkins:/var/jenkins_home --device /dev/ttyUSB0 --device /dev/ttyUSB1 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker jenkins-arm-iot
-
